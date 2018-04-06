cc4ce552423b15218022ea9c5919665b545d167a
Sebastian Skałacki <skalee@gmail.com>
2018-04-06 18:17:20 +0200

    Load support files in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require "enmail"
 require "pry"
 
+Dir[File.expand_path "../support/**/*.rb", __FILE__].sort.each { |f| require f }
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
