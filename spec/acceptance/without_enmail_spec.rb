3aaedbfb8526804b9cc9d99debb448b1805fbc87
Sebastian Skałacki <skalee@gmail.com>
2018-04-22 03:43:46 +0200

    Extract common acceptance expectations
    Avoid code repetition by extracting common expectations on processed
    e-mails into helper methods.

--- spec/acceptance/without_enmail_spec.rb
+++ spec/acceptance/without_enmail_spec.rb
@@ -7,45 +7,31 @@ RSpec.describe "Sending without protection (unaltered by EnMail)" do
     mail = simple_mail
 
     mail.deliver
-    expect(mail.from).to contain_exactly(mail_from)
-    expect(mail.to).to contain_exactly(mail_to)
-    expect(mail.mime_type).to eq("text/plain")
-    expect(mail.body.decoded).to eq(mail_text)
+    common_message_expectations(mail)
+    decrypted_part_expectations_for_simple_mail(mail)
   end
 
   specify "a non-multipart HTML message" do
     mail = simple_html_mail
 
     mail.deliver
-    expect(mail.from).to contain_exactly(mail_from)
-    expect(mail.to).to contain_exactly(mail_to)
-    expect(mail.mime_type).to eq("text/html")
-    expect(mail.body.decoded).to eq(mail_html)
+    common_message_expectations(mail)
+    decrypted_part_expectations_for_simple_html_mail(mail)
   end
 
   specify "a multipart text+HTML message" do
     mail = text_html_mail
 
     mail.deliver
-    expect(mail.from).to contain_exactly(mail_from)
-    expect(mail.to).to contain_exactly(mail_to)
-    expect(mail.body.raw_source).to be_empty
-    expect(mail.parts[0].mime_type).to eq("text/plain")
-    expect(mail.parts[0].body.decoded).to eq(mail_text)
-    expect(mail.parts[1].mime_type).to eq("text/html")
-    expect(mail.parts[1].body.decoded).to eq(mail_html)
+    common_message_expectations(mail)
+    decrypted_part_expectations_for_text_html_mail(mail)
   end
 
   specify "a multipart message with binary attachments" do
     mail = text_jpeg_mail
 
     mail.deliver
-    expect(mail.from).to contain_exactly(mail_from)
-    expect(mail.to).to contain_exactly(mail_to)
-    expect(mail.body.raw_source).to be_empty
-    expect(mail.parts[0].mime_type).to eq("text/plain")
-    expect(mail.parts[0].body.decoded).to eq(mail_text)
-    expect(mail.parts[1].mime_type).to eq("image/jpeg")
-    expect(mail.parts[1].body.decoded).to eq(SMALLEST_JPEG)
+    common_message_expectations(mail)
+    decrypted_part_expectations_for_text_jpeg_mail(mail)
   end
 end
--- spec/support/example_emails.rb
+++ spec/support/example_emails.rb
@@ -15,6 +15,11 @@ shared_context "example emails" do
     m
   end
 
+  def decrypted_part_expectations_for_simple_mail(message_or_part)
+    expect(message_or_part.mime_type).to eq("text/plain")
+    expect(message_or_part.body.decoded).to eq(mail_text)
+  end
+
   let(:simple_html_mail) do
     m = Mail.new
     m.from = mail_from
@@ -24,6 +29,11 @@ shared_context "example emails" do
     m.body = mail_html
     m.content_type = "text/html"
     m
+  end
+
+  def decrypted_part_expectations_for_simple_html_mail(message_or_part)
+    expect(message_or_part.mime_type).to eq("text/html")
+    expect(message_or_part.body.decoded).to eq(mail_html)
   end
 
   let(:text_html_mail) do
@@ -37,6 +47,14 @@ shared_context "example emails" do
     m
   end
 
+  def decrypted_part_expectations_for_text_html_mail(message_or_part)
+    expect(message_or_part.body.raw_source).to be_empty
+    expect(message_or_part.parts[0].mime_type).to eq("text/plain")
+    expect(message_or_part.parts[0].body.decoded).to eq(mail_text)
+    expect(message_or_part.parts[1].mime_type).to eq("text/html")
+    expect(message_or_part.parts[1].body.decoded).to eq(mail_html)
+  end
+
   let(:text_jpeg_mail) do
     m = Mail.new
     m.from = mail_from
@@ -46,5 +64,19 @@ shared_context "example emails" do
     m.body = mail_text
     m.add_file filename: "pic.jpg", content: SMALLEST_JPEG
     m
+  end
+
+  def decrypted_part_expectations_for_text_jpeg_mail(message_or_part)
+    expect(message_or_part.body.raw_source).to be_empty
+    expect(message_or_part.parts[0].mime_type).to eq("text/plain")
+    expect(message_or_part.parts[0].body.decoded).to eq(mail_text)
+    expect(message_or_part.parts[1].mime_type).to eq("image/jpeg")
+    expect(message_or_part.parts[1].body.decoded).to eq(SMALLEST_JPEG)
+  end
+
+  def common_message_expectations(message)
+    expect(message.from).to contain_exactly(mail_from)
+    expect(message.to).to contain_exactly(mail_to)
+    expect(message.subject).to eq(mail_subject)
   end
 end
