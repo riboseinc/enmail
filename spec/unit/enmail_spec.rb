cb049e63dd79006fecbcfacb6df84123fb514d14
Sebastian Skałacki <skalee@gmail.com>
2018-04-22 04:41:01 +0200

    Implement EnMail.protect method
    Implement a method which will actually sign/encrypt messages, an EnMail
    main entry point.
    
    There is no need to care about options yet.

---
+++ spec/unit/enmail_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe EnMail do
+  describe "::protect" do
+    subject { EnMail.method(:protect) }
+
+    let(:adapter_class) { EnMail::Adapters::GPGME }
+    let(:adapter_dbl) { instance_double(adapter_class, sign: nil) }
+    let(:message) { Mail.new }
+
+    before { allow(adapter_class).to receive(:new).and_return(adapter_dbl) }
+
+    it "instantiates an adapter with proper options" do
+      subject.call :sign, message, proper: :options
+      expect(adapter_class).to have_received(:new).with(proper: :options)
+    end
+
+    it "calls indicated method, passing a message as an argument" do
+      subject.call :sign, message
+      expect(adapter_dbl).to have_received(:sign).with(message)
+    end
+  end
+end
