324512ac74ad457a8ab6a63642dff4c8c8488e48
Sebastian Skałacki <skalee@gmail.com>
2018-04-28 11:54:51 +0200

    Catch GPG signature parse errors

--- spec/support/matchers/be_a_valid_pgp_signature_of.rb
+++ spec/support/matchers/be_a_valid_pgp_signature_of.rb
@@ -1,8 +1,11 @@
 RSpec::Matchers.define :be_a_valid_pgp_signature_of do |text|
   match do |signature|
     validity = false
-    ::GPGME::Crypto.new.verify(signature, signed_text: text) do |sig|
-      validity = sig.valid?
+    begin
+      ::GPGME::Crypto.new.verify(signature, signed_text: text) do |sig|
+        validity = sig.valid?
+      end
+    rescue GPGME::Error::NoData # Signature parse error
     end
     validity
   end
