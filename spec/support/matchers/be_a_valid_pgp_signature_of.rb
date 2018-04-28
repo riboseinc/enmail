9ea41265eb7046b6e6d85f20c030dcb12b6c38b3
Sebastian Skałacki <skalee@gmail.com>
2018-04-28 13:54:17 +0200

    Refactor be_a_valid_pgp_signature_of matcher
    Extract some validation logic into a method.

--- spec/support/matchers/be_a_valid_pgp_signature_of.rb
+++ spec/support/matchers/be_a_valid_pgp_signature_of.rb
@@ -1,18 +1,20 @@
 RSpec::Matchers.define :be_a_valid_pgp_signature_of do |text|
   match do |signature|
-    @msg = nil
-    begin
-      ::GPGME::Crypto.new.verify(signature, signed_text: text) do |sig|
-        @msg = msg_mismatch(text) unless sig.valid?
-      end
-    rescue GPGME::Error::NoData # Signature parse error
-      @msg = msg_no_gpg_sig(signature)
-    end
+    @msg = validate_signature(signature, text)
     @msg.nil?
   end
 
   failure_message do
     @msg
+  end
+
+  # Returns +nil+ if signature is valid, or an error message otherwise.
+  def validate_signature(signature, text)
+    ::GPGME::Crypto.new.verify(signature, signed_text: text) do |sig|
+      msg_mismatch(text) unless sig.valid?
+    end
+  rescue GPGME::Error::NoData # Signature parse error
+    msg_no_gpg_sig(signature)
   end
 
   def msg_mismatch(text)
