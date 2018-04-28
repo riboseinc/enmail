a1ef851c4b5f67bc782a75069a7045992e81d44a
Sebastian Skałacki <skalee@gmail.com>
2018-04-28 13:50:51 +0200

    Write nice failure messages for a RSpec matcher

--- spec/support/matchers/be_a_valid_pgp_signature_of.rb
+++ spec/support/matchers/be_a_valid_pgp_signature_of.rb
@@ -1,12 +1,27 @@
 RSpec::Matchers.define :be_a_valid_pgp_signature_of do |text|
   match do |signature|
-    validity = false
+    @msg = nil
     begin
       ::GPGME::Crypto.new.verify(signature, signed_text: text) do |sig|
-        validity = sig.valid?
+        @msg = msg_mismatch(text) unless sig.valid?
       end
     rescue GPGME::Error::NoData # Signature parse error
+      @msg = msg_no_gpg_sig(signature)
     end
-    validity
+    @msg.nil?
+  end
+
+  failure_message do
+    @msg
+  end
+
+  def msg_mismatch(text)
+    "expected given signature to be a valid Open PGP signature " +
+      "of following text:\n#{text}"
+  end
+
+  def msg_no_gpg_sig(sig_text)
+    "expected given text to be a valid Open PGP signature, " +
+      "but it contains no signature data, just:\n#{sig_text}"
   end
 end
