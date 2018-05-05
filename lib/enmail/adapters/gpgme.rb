0fdb90c59122e5260210fd9275f2e6968b5292db
Sebastian Skałacki <skalee@gmail.com>
2018-05-06 00:30:39 +0200

    Move a #body_to_part method
    Move a #body_to_part method to MessageManipulation helper.  Add some
    documentation, and change visibility from private to protected.

--- lib/enmail/adapters/gpgme.rb
+++ lib/enmail/adapters/gpgme.rb
@@ -45,30 +45,6 @@ module EnMail
 
       private
 
-      # Returns a new +Mail::Part+ with the same content and MIME headers
-      # as the message passed as an argument.
-      #
-      # Although Mail gem provides +Mail::Message#convert_to_multipart+ method,
-      # it works correctly for non-multipart text/plain messages only.  This
-      # method is more robust, and handles messages containing any content type,
-      # be they multipart or not.
-      #
-      # The message passed as an argument is not altered.
-      #
-      # TODO Copy MIME headers (ones which start with "Content-")
-      # TODO Preserve Content-Transfer-Encoding when possible
-      # TODO Set some safe Content-Transfer-Encoding, like quoted-printable
-      def body_to_part(message)
-        part = ::Mail::Part.new
-        part.content_type = message.content_type
-        if message.multipart?
-          message.body.parts.each { |p| part.add_part p.dup }
-        else
-          part.body = message.body.decoded
-        end
-        part
-      end
-
       def build_signature_part(part_to_sign, signer)
         signature = compute_signature(part_to_sign.encoded, signer).to_s
         part = ::Mail::Part.new
