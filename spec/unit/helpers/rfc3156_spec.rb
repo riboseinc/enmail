510b765e45663fe1ae2ec6fc4608f8997a69501e
Sebastian Skałacki <skalee@gmail.com>
2018-05-09 00:49:11 +0200

    Write specs for #sign_and_encrypt_encapsulated

--- spec/unit/helpers/rfc3156_spec.rb
+++ spec/unit/helpers/rfc3156_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe EnMail::Helpers::RFC3156 do
   let(:options) { {} }
 
   include_context "example emails"
+
+  describe "#sign_and_encrypt_encapsulated" do
+    subject { adapter.method(:sign_and_encrypt_encapsulated) }
+
+    let(:mail) { simple_mail }
+
+    it "signs and encrypts the message, in that exact order" do
+      expect(adapter).to receive(:sign).with(mail).ordered
+      expect(adapter).to receive(:encrypt).with(mail).ordered
+      subject.(mail)
+    end
+  end
 end
