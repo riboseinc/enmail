e34ce7be1dd65f9c5b0d5c1c0400fc3332172bfd
Sebastian Skałacki <skalee@gmail.com>
2018-07-09 14:50:10 +0200

    Keep a GPGME home path in a constant
    Make this value available outside the support file, particularly in
    matchers.

--- spec/support/gpgme_setup.rb
+++ spec/support/gpgme_setup.rb
@@ -2,6 +2,6 @@
 # on file name length of the socket, and GPGME makes use of UNIX sockets.
 # Directory name produced by +Dir.mktmpdir+ is often quite long, and that
 # may cause weird error with misleading message.
-tmp_gpgme_home = File.expand_path("../../tmp/gpgme", __dir__)
-FileUtils.mkdir_p(tmp_gpgme_home)
-GPGME::Engine.home_dir = tmp_gpgme_home
+TMP_GPGME_HOME = File.expand_path("../../tmp/gpgme", __dir__)
+FileUtils.mkdir_p(TMP_GPGME_HOME)
+GPGME::Engine.home_dir = TMP_GPGME_HOME
