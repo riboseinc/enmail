0206ff08c62feeb0cf289cfdafcee5e31531cee9
Sebastian Skałacki <skalee@gmail.com>
2018-07-23 19:10:06 +0200

    Support different adapters
    Support arbitrary cipher adapters, including GPGME.  Adapter must be
    specified explicitly, there are no defaults for now.

--- spec/unit/enmail_spec.rb
+++ spec/unit/enmail_spec.rb
@@ -2,19 +2,19 @@ RSpec.describe EnMail do
   describe "::protect" do
     subject { EnMail.method(:protect) }
 
-    let(:adapter_class) { EnMail::Adapters::GPGME }
-    let(:adapter_dbl) { instance_double(adapter_class, sign: nil) }
+    let(:adapter_class) { Class.new }
+    let(:adapter_dbl) { double(sign: nil) }
     let(:message) { Mail.new }
 
     before { allow(adapter_class).to receive(:new).and_return(adapter_dbl) }
 
     it "instantiates an adapter with proper options" do
-      subject.call :sign, message, proper: :options
+      subject.call :sign, message, adapter: adapter_class, proper: :options
       expect(adapter_class).to have_received(:new).with(proper: :options)
     end
 
     it "calls indicated method on adapter, passing a message as an argument" do
-      subject.call :sign, message
+      subject.call :sign, message, adapter: adapter_class
       expect(adapter_dbl).to have_received(:sign).with(message)
     end
   end
