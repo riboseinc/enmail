578ed0d87ed95fe6ea120425c0c58fc8df73bc49
Sebastian Skałacki <skalee@gmail.com>
2018-07-23 19:10:06 +0200

    Support different adapters
    Support arbitrary cipher adapters, including GPGME.  Adapter must be
    specified explicitly, there are no defaults for now.

--- lib/enmail.rb
+++ lib/enmail.rb
@@ -12,8 +12,8 @@ require "enmail/adapters/gpgme"
 module EnMail
   module_function
 
-  def protect(mode, message, **options)
-    adapter = Adapters::GPGME.new(options)
-    adapter.public_send mode, message
+  def protect(mode, message, adapter:, **options)
+    adapter_obj = adapter.new(options)
+    adapter_obj.public_send mode, message
   end
 end
