6aa27beb0f3495ad7a2b755db40a30b1d1327ffb
Sebastian Skałacki <skalee@gmail.com>
2018-07-23 19:25:10 +0200

    Override Rnp's home directory in specs

---
+++ spec/support/rnp_setup.rb
@@ -0,0 +1,10 @@
+# Keep tmp directory as short as possible.  UNIX has some annoying limit
+# on file name length of the socket, and GPGME makes use of UNIX sockets.
+# Directory name produced by +Dir.mktmpdir+ is often quite long, and that
+# may cause weird error with misleading message.
+TMP_RNP_HOME = File.expand_path("../../tmp/gpgme", __dir__)
+FileUtils.mkdir_p(TMP_RNP_HOME)
+
+def Rnp.default_homedir
+  TMP_RNP_HOME
+end
