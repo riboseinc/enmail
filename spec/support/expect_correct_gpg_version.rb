6c36c1cd0ac1d5da2a44a19f2f5be0ce74bb9bc2
Sebastian Skałacki <skalee@gmail.com>
2018-07-24 22:01:27 +0200

    Expect the correct GnuPG version in specs
    Ensure that intended GPG version is actually used instead of the system
    one.  Fail build if not.

---
+++ spec/support/expect_correct_gpg_version.rb
@@ -0,0 +1,18 @@
+expected_gpg_version = ENV.fetch("EXPECT_GPG_VERSION", nil)
+
+if expected_gpg_version
+  gpg_version_info = ::GPGME::Engine.info.detect do |ei|
+    %r"/bin/gpg\d*\Z" =~ ei.file_name
+  end
+
+  actual_gpg_version = gpg_version_info.version
+  expected_gpg_version_rx = /\A#{Regexp.escape(expected_gpg_version)}(\.|\Z)/
+
+  if expected_gpg_version_rx =~ actual_gpg_version
+    puts "This test suite is run against GPG version #{actual_gpg_version}."
+  else
+    raise "This test suite was expected to run with GPG version " +
+      "#{expected_gpg_version}, but was attempted to run with GPG version " +
+      "#{actual_gpg_version}.  Aborting!"
+  end
+end
