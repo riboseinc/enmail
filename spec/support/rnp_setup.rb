87acab22e4010e58bc74bd6da99d26e4e83a4bea
Sebastian Skałacki <skalee@gmail.com>
2018-08-06 03:50:07 +0200

    Change development PGP home location
    Previous `tmp/gpgme` was somewhat misleading.  It's a common development
    home directory for all OpenPGP implementations supported by EnMail.
    `tmp/pgp_home` is much better.

--- spec/support/rnp_setup.rb
+++ spec/support/rnp_setup.rb
@@ -2,7 +2,7 @@
 # on file name length of the socket, and GPGME makes use of UNIX sockets.
 # Directory name produced by +Dir.mktmpdir+ is often quite long, and that
 # may cause weird error with misleading message.
-TMP_RNP_HOME = File.expand_path("../../tmp/gpgme", __dir__)
+TMP_RNP_HOME = File.expand_path("../../tmp/pgp_home", __dir__)
 FileUtils.mkdir_p(TMP_RNP_HOME)
 
 def Rnp.default_homedir
