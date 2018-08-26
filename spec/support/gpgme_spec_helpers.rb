215052ea506ac65e3bae82ab2b65a2b05d3eb1a6
Sebastian Skałacki <skalee@gmail.com>
2018-08-26 13:39:21 +0200

    Do not depend on GPGME in specs
    GPGME is an optional dependency, and some test builds will be run
    without presence of this library.  Therefore, no test (unless it's
    specific for GPGME adapter) should depend on it.
    
    RSpec PGP Matchers provide GPGRunner which can be used instead.

--- spec/support/gpgme_spec_helpers.rb
+++ spec/support/gpgme_spec_helpers.rb
@@ -3,8 +3,9 @@
 
 shared_context "gpgme spec helpers" do
   def decrypt_mail(message)
+    gpg_runner = RSpec::PGPMatchers::GPGRunner
     encrypted_message = message.parts[1].body.decoded
-    decrypted_raw_message = GPGME::Crypto.new.decrypt(encrypted_message)
+    decrypted_raw_message, = gpg_runner.run_decrypt(encrypted_message)
     Mail::Part.new(decrypted_raw_message)
   end
 
