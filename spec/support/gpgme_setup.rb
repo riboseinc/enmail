2d373be508a74cebfe53b404c3ade580c22a6410
Sebastian Skałacki <skalee@gmail.com>
2019-03-13 14:24:01 +0100

    Fix specs
    Optional dependencies are lazily loaded now, hence checking for constant
    presence will fail even if given dependency can be loaded.

--- spec/support/gpgme_setup.rb
+++ spec/support/gpgme_setup.rb
@@ -1,6 +1,7 @@
 # (c) Copyright 2018 Ribose Inc.
 #
 
-if defined?(::GPGME)
+unless ENV["TEST_WITHOUT_GPGME"]
+  require "gpgme"
   GPGME::Engine.home_dir = TMP_PGP_HOME
 end
--- spec/support/rnp_setup.rb
+++ spec/support/rnp_setup.rb
@@ -1,7 +1,9 @@
 # (c) Copyright 2018 Ribose Inc.
 #
 
-if defined?(::Rnp)
+unless ENV["TEST_WITHOUT_RNP"]
+  require "rnp"
+
   def Rnp.default_homedir
     TMP_PGP_HOME
   end
