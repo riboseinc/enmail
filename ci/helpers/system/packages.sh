71a7b733dd2c89627ef80e2b374c902abae30d05
Sebastian Skałacki <skalee@gmail.com>
2019-06-12 03:04:41 +0200

    xen

--- ci/helpers/system/packages.sh
+++ ci/helpers/system/packages.sh
@@ -6,14 +6,25 @@ apt-get update
 # If you want custom PPAs, you need following.
 apt-get install -y software-properties-common
 
-# Build Essential is a collection of compilers and build tools.
-# Git is obligatory for obvious reasons.
-# bzip2 provides bzip2
-# cURL is necessary to download RVM.
-# Make is optional, and RVM installs it anyway, but I prefer to have it early on
 # Sudo is sudo…
-# Vim is optional, but I really love it.
-apt-get install -y build-essential bzip2 curl git make sudo vim
+apt-get install -y sudo
 
+# Following are required build tools:
+# Autoconf is required to build GnuPG and RNP from Git sources.
+# Build Essential is a collection of compilers and build tools.
+# bzip2 is required to unpack GnuPG source packages.
+# CMake is required to build RNP.
+# cURL is necessary to download RVM.
+# Git is obligatory for obvious reasons.
+# Libtool is required to build RNP from Git sources.
+apt-get install -y autoconf build-essential bzip2 cmake curl git libtool
 
-apt-get install -y autoconf gcc g++ libbz2-dev libncurses5-dev libtool python wget zlib1g-dev
+# Some GUI toolkit or framework is required to build GnuPG's Pinentry, and
+# ncurses is the least demanding option.
+apt-get install -y libncurses5-dev
+
+# These are RNP dependencies
+apt-get install -y libbotan-2-dev libbz2-dev libjson-c-dev zlib1g-dev
+
+# Vim is entirely optional, but I really love to have it.
+apt-get install -y vim
